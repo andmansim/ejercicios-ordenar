ce0f0522d31f1ee330f5b6adfdc59626c70dd4f5
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-01 21:32:09 +0100

    ordenar una cadena

--- dicotomia.py
+++ dicotomia.py
@@ -1,6 +1,20 @@
 #Variables
-t = [1, 6, 2, 4, 7, 3, 9, 5, 8 ]
-inicio = t[0]
+t = [6, 7, 2, 4, 1, 3, 9, 5, 8 ]
+
 a = len(t)
-fin = t[a - 1]
+
+def comparar():
+  
+    for i in range(1, a ):
+        inicial = t[i]
+                       
+        while  i > 0 and t[i -1] > inicial:
+            t[i] = t[i - 1]
+            i=i -1
+            t[i] = inicial
+            print (t)    
+        print(t)
+            
+
+comparar()
 
