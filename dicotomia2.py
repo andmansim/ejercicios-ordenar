6cf8a1823fb096d0eccedcecef59285523057eda
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-02 19:25:53 +0100

    comentarios y función

--- dicotomia2.py
+++ dicotomia2.py
@@ -1,46 +1,15 @@
 #Variables
 t = [2, 6, 7, 8, 3, 5, 1, 9, 0]
-r = []
-'''a = t.copy()
-def comparar():
-    for i in range(1, len(t)): #Empezamos con el 1 para poderlo comparar con el posterior y con el siguiente
-        inicial = t[i]
-        k = i 
-        if t[k - 1] > inicial:  #filtro para añadir solo al que ordenamos       
-            while  k > 0 and t[k - 1] > inicial:
-                t[k] = t[k - 1]
-                k = k - 1  #Hacemos que retroceda para no saltarnos ningún valor
-                t[k] = inicial 
-                
-            #r.append(inicial)
-            #r.sort() #nueva tabla ordenada
-       # else: #añadimos los elementos que van en orden creciente y los ordenamos
-            #r.append(t[k - 1])
-        print (t)    
-        r.append(t[i])        
-   # if len(t) == len(r): #comprobación de que ambas listas tienen los mismos elementos
-        #pass
-    #else:
-        #r.append(t[len(t) - 1])
-    
-comparar()
+r = [None] * len(t) #tabla auxiliar con el número de elementos que t
 
-print("tabla t")
-print (a)    
-print("tabla r")
-print(r)    
-    '''
-r = ['','','','','','','','','']    
-for i in range (0, len(t)):
-    m = 0
-    for k in range(0, len(t)):
-        #if t[i] < t[i - 1]:
-           
-        if t[k] < t[i]:
-            #print(k)
-            m = m + 1
-                         
-    print (m, t[i])
-    r[m] = t[i]
-    print (r)
-    
+def ordenaaux():
+    for i in range (0, len(t)):
+        m = 0
+        for k in range(0, len(t)):
+        
+            if t[k] < t[i]: #Vemos los elementos menores que el que comparamos
+                m = m + 1 
+                            
+        r[m] = t[i] #añadimos los elementos en sus respectivas posicones
+        print (r)
+ordenaaux()
