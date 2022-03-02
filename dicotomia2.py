0ab69b09a4b62ff133ef2c23b67994ca193e6493
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-02 19:21:02 +0100

    cambios y nueva manera

--- dicotomia2.py
+++ dicotomia2.py
@@ -1,7 +1,7 @@
 #Variables
 t = [2, 6, 7, 8, 3, 5, 1, 9, 0]
 r = []
-c = t
+'''a = t.copy()
 def comparar():
     for i in range(1, len(t)): #Empezamos con el 1 para poderlo comparar con el posterior y con el siguiente
         inicial = t[i]
@@ -12,20 +12,35 @@ def comparar():
                 k = k - 1  #Hacemos que retroceda para no saltarnos ningún valor
                 t[k] = inicial 
                 
-            r.append(inicial)
-            r.sort() #nueva tabla ordenada
-        else: #añadimos los elementos que van en orden creciente y los ordenamos
-            r.append(t[k - 1])
-            
-    if len(t) == len(r): #comprobación de que ambas listas tienen los mismos elementos
-        pass
-    else:
-        r.append(t[len(t) - 1])
-    t = c
+            #r.append(inicial)
+            #r.sort() #nueva tabla ordenada
+       # else: #añadimos los elementos que van en orden creciente y los ordenamos
+            #r.append(t[k - 1])
+        print (t)    
+        r.append(t[i])        
+   # if len(t) == len(r): #comprobación de que ambas listas tienen los mismos elementos
+        #pass
+    #else:
+        #r.append(t[len(t) - 1])
+    
 comparar()
 
 print("tabla t")
-print (t)    
+print (a)    
 print("tabla r")
 print(r)    
+    '''
+r = ['','','','','','','','','']    
+for i in range (0, len(t)):
+    m = 0
+    for k in range(0, len(t)):
+        #if t[i] < t[i - 1]:
+           
+        if t[k] < t[i]:
+            #print(k)
+            m = m + 1
+                         
+    print (m, t[i])
+    r[m] = t[i]
+    print (r)
     
