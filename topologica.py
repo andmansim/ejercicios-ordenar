866c71d70ae6f30007fb3d06e0973cc7977b070f
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-03 22:27:50 +0100

    intento 2

--- topologica.py
+++ topologica.py
@@ -1,6 +1,8 @@
 #Variables
 tareas2=[[1,7],[1,3],[1,9],[2,4],[2,8],[3,6],[3,4],[4,"-"],[5,1],[6,"-"],[7,"-"],[8,"-"],[9,2]]
-dependencias =[[1,3],[2,2],[3,2],[4,0],[5,1],[6,0],[7,0],[8,0],[9,1]]
+print (tareas2)
+
+dependencias =dict([[1,3],[2,2],[3,2],[4,0],[5,1],[6,0],[7,0],[8,0],[9,1]])
 
 t_ejecutada = [None] * len(tareas2)
 #Nos lee la lista y los elementos padres nos lo apunta en una lista
@@ -12,24 +14,25 @@ for i in range(0,len(tareas2)-1):
         num_tarea = num_tarea + 1
 print(t_ejecutada)
 c = 0
-print (tareas2.index (tareas2[2],[8]))
+
 def ejecutarTarea(r, c,num_tarea):
     #tarea_ejecutar=tareas2[r][0]
-    
-    if tareas2[r][1] in t_ejecutada and c == dependencias[r][1]:
+    r4=dependencias.get(r) #no funciona esta linea
+    if tareas2[r][1] in t_ejecutada and c == r4:
         #buscamos a siguiente dependencia
         #ejecutarTarea (r+1)
         #c=c+1
         
         t_ejecutada.insert(num_tarea,tareas2[r][0])
         num_tarea = num_tarea + 1
-        
-        
+       
+     
     elif num_tarea < 8: 
        #ejecutarTarea (r+1, c + 1,num_tarea)
        #c = c + 1
-       r2 = tareas2.index (tareas2[r][1])
+       r2 =dependencias.get(r)
        ejecutarTarea (r2,c+1,num_tarea)
+     
         
 ejecutarTarea(0, 0,num_tarea)
 print(t_ejecutada)
@@ -38,3 +41,10 @@ print(t_ejecutada)
 
 
             
+
+     
+    
+
+
+            
+    
