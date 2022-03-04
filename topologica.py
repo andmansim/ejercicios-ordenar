5eff1eca5f4f3034d1ee0b5fa0b10171018e0475
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-04 18:29:42 +0100

    cambio a diccionario

--- topologica.py
+++ topologica.py
@@ -1,29 +1,25 @@
 #Variables
-tareas2=[[1,7],[1,3],[1,9],[2,4],[2,8],[3,6],[3,4],[4,"-"],[5,1],[6,"-"],[7,"-"],[8,"-"],[9,2]]
-print (tareas2)
-
-dependencias =dict([[1,3],[2,2],[3,2],[4,0],[5,1],[6,0],[7,0],[8,0],[9,1]])
-
-t_ejecutada = [None] * len(tareas2)
+tareas = {1: [7, 3, 9], 2: [4,8], 3: [6,4], 4:[], 5: [1], 6:[], 7:[], 8:[],9: [2]} #lista de tareas y sus dependencias
+t_ejecutada = [] # Lista donde se guardan las tareas ya ejecutadas
 #Nos lee la lista y los elementos padres nos lo apunta en una lista
 num_tarea=0
-for i in range(0,len(tareas2)-1):
+for i in range(0,len(tareas)-1):
     
-    if tareas2[i][1] == "-":
-        t_ejecutada.insert(num_tarea,tareas2[i][0])
+    if tareas[i][1] == "-":
+        t_ejecutada.insert(num_tarea,tareas[i][0])
         num_tarea = num_tarea + 1
 print(t_ejecutada)
 c = 0
 
 def ejecutarTarea(r, c,num_tarea):
     #tarea_ejecutar=tareas2[r][0]
-    r4=dependencias.get(r) #no funciona esta linea
-    if tareas2[r][1] in t_ejecutada and c == r4:
+    
+    if tareas[r][1] in t_ejecutada and c == dependencias.get(r):
         #buscamos a siguiente dependencia
         #ejecutarTarea (r+1)
         #c=c+1
         
-        t_ejecutada.insert(num_tarea,tareas2[r][0])
+        t_ejecutada.insert(num_tarea,tareas[r][0])
         num_tarea = num_tarea + 1
        
      
