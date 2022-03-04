33cbff7cc664932487ae28906ab6e1213f8500aa
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-04 18:59:57 +0100

    intento 3

--- topologica.py
+++ topologica.py
@@ -2,23 +2,21 @@
 tareas = {1: [7, 3, 9], 2: [4,8], 3: [6,4], 4:[], 5: [1], 6:[], 7:[], 8:[],9: [2]} #lista de tareas y sus dependencias
 t_ejecutada = [] # Lista donde se guardan las tareas ya ejecutadas
 #Nos lee la lista y los elementos padres nos lo apunta en una lista
-
-
-def ejecutarTarea():
-    for j in tareas():
+def ejecutarTarea(r):
+    for j in tareas [r]:
         if j not in t_ejecutada:
             ejecutarTarea(j)
-        print(t_ejecutada)
- 
-def buscar(w):           
-    for i in tareas[w]:
-        if i not in t_ejecutada:
-            buscar(i)
-    print(t_ejecutada)
-
-
+    t_ejecutada.insert(0, j)
+            
+for i in range(1,len(tareas)):
+    if i not in t_ejecutada:
+        ejecutarTarea (i)
+   
+print(t_ejecutada)
+      
+     
                
-ejecutarTarea(0)
+ejecutarTarea(1)
 print(t_ejecutada)
      
     
