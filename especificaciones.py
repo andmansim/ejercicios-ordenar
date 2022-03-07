486acbdda9b0ae679d01c5862ae0158bce407e3c
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-07 20:14:19 +0100

    primera parte

--- especificaciones.py
+++ especificaciones.py
@@ -4,3 +4,19 @@ inicio = 0
 fin = len(t) - 1
 segmento = [] #se irá guardando los distintos segmentos que encontremos 
 segmento_final = [] #mostrará el resultado final
+
+# Función que divide en segmentos nuestra lista con el número máximo al principio
+def segmentos ():
+    segmento = []
+    e = 0
+    s_total = [] #lsita que contiene a todos los segmentos
+    for k in range (i, fin + 1):
+        if t[k] > t[i]:
+            for j in range(i, k):
+                segmento.append(t[j])
+            s_total.append(segmento)
+            segmento = []
+            e = k
+            i = k
+            
+    
