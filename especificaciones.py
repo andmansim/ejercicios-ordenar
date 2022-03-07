43bd34144f832aeef5174cf1ad5155fec51d7c23
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-07 20:54:35 +0100

    intento 2

--- especificaciones.py
+++ especificaciones.py
@@ -11,14 +11,16 @@ def segmentos (i):
     e = 0
     s_total = [] #lsita que contiene a todos los segmentos
     for k in range (i, fin + 1):
+        
         if t[k] > t[i]: # solo pasa aquellos que son mayor que el primero
-            
+            print(t[k])
             for j in range(i, k): # los añadimos a segmento
                 segmento.append(t[j])
                 
             s_total.append(segmento) # añadimos el segmento al total
-            segmentos(k)
+            
             segmento = []
+            segmentos(k)
             e = k # nos guarda el último valor de k para luego empezar con él
             i = k
         
