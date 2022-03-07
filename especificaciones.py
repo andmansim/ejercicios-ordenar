ca0c37c2e007fc0058c1d0305b2c952778e9d379
andmansim <91722324+andmansim@users.noreply.github.com>
2022-03-07 20:08:45 +0100

    varaibles

---
+++ especificaciones.py
@@ -0,0 +1,6 @@
+# Variables
+t = [14, 7, 12, 6, 18, 13, 9, 10, 16, 21, 19, 8, 25, 3]
+inicio = 0
+fin = len(t) - 1
+segmento = [] #se irá guardando los distintos segmentos que encontremos 
+segmento_final = [] #mostrará el resultado final
